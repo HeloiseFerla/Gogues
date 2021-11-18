8d7eb2e3d3f15b8b1b6acbe9c3c44d1e99c30c1d
HeloiseFerla <ferla.heloise@gmail.com>
2021-11-18 13:43:45 +0100

    Add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import './style.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home';
 import Header from './components/header';
+import Bars from './components/bars';
+import Restaurants from './components/restaurants';
+import './style.css';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="restaurants" element={<Restaurants />} />
+        <Route path="bars" element={<Bars />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
