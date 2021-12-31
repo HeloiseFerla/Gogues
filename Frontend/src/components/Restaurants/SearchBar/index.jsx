aecc025f021ffd4118845711c0d950d152f8d70d
HeloiseFerla <ferla.heloise@gmail.com>
2021-12-31 14:35:55 +0100

    Add autocomplete to the searchbar

--- Frontend/src/components/Restaurants/SearchBar/index.jsx
+++ Frontend/src/components/Restaurants/SearchBar/index.jsx
@@ -1,63 +1,73 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import SSearchBar from './style';
 
-export default function SearchBar({
-  setResults,
-  setAverageLat,
-  setAverageLon,
-}) {
-  const [searchCity, setSearchCity] = useState('Paris');
-
-  const average = (array, key) => {
-    let sum = 0;
-    if (key === 'lat') {
-      sum = array.reduce((previousValue, currentValue) => {
-        return previousValue.lat + currentValue.lat;
-      });
-    } else {
-      sum = array.reduce((previousValue, currentValue) => {
-        return previousValue.lon + currentValue.lon;
-      });
-    }
-    return sum / array.length;
-  };
+export default function SearchBar({ setCity }) {
+  const [search, setSearch] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [autoResults, setAutoResults] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/restaurants?city=${searchCity}`)
+      .get(`http://localhost:5000/cities`)
+
       .then(({ data }) => {
-        setResults(data);
-        setAverageLat(average(data, 'lat'));
-        setAverageLon(average(data, 'lon'));
+        const tempcities = data.map((obj) => {
+          return obj.name;
+        });
+        setCities([...tempcities]);
       })
       .catch((e) => {
         console.log(e);
       });
-  }, [searchCity]);
+  }, []);
 
-  const handleCityChange = (e) => {
-    setSearchCity(e.target.value);
+  useEffect(() => {
+    if (search.length) {
+      setAutoResults(
+        cities.filter((name) =>
+          name.toUpperCase().includes(search.toUpperCase())
+        )
+      );
+    } else {
+      setAutoResults([]);
+    }
+  }, [search]);
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
   };
   return (
     <SSearchBar>
       <input
         type="search"
-        value={searchCity}
-        onChange={handleCityChange}
-        placeholder="Paris"
+        value={search}
+        onChange={handleSearchChange}
+        placeholder="Lille"
       />
+      <ul>
+        {autoResults.map((res) => {
+          return (
+            <li key={res}>
+              <button
+                onClick={() => {
+                  setCity(res);
+                }}
+                type="button"
+              >
+                {res}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </SSearchBar>
   );
 }
 SearchBar.propTypes = {
-  setResults: PropTypes.func,
-  setAverageLat: PropTypes.func,
-  setAverageLon: PropTypes.func,
+  setCity: PropTypes.func,
 };
 SearchBar.defaultProps = {
-  setResults: () => {},
-  setAverageLat: () => {},
-  setAverageLon: () => {},
+  setCity: () => {},
 };
--- Frontend/src/components/Restaurants/SearchBar/style.jsx
+++ Frontend/src/components/Restaurants/SearchBar/style.jsx
@@ -1,4 +1,27 @@
 import styled from 'styled-components';
 
-const SSearchBar = styled.form``;
+const SSearchBar = styled.div`
+  input {
+    :focus {
+      outline: none;
+    }
+  }
+  ul {
+    max-height: 200px;
+    overflow: scroll;
+
+    li {
+      padding: 5px 0;
+      :hover {
+        background-color: var(--color-light);
+      }
+      border-top: 1px solid var(--color-light);
+      button {
+        height: 100%;
+        width: 100%;
+        text-align: left;
+      }
+    }
+  }
+`;
 export default SSearchBar;
